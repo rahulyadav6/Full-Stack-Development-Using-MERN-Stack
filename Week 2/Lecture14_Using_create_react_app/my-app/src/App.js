94dc6fdaa2187dcd183995a302222bc8fe4cb160
Rahul <ry573870@gmail.com>
2024-07-02 10:20:39 +0530

    Understanding React Components

--- Week 2/Lecture14_Using_create_react_app/my-app/src/App.js
+++ Week 2/Lecture14_Using_create_react_app/my-app/src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 
-function App() {
-  const Name = "Rahul";
-  return (
-    <>
-      <h1 style={{
-        color:"red",
-        textAlign: "center",
-      }}>Hello from {Name}!</h1>
-    </>
-  );
+import {Component} from "react"
+
+class App extends Component{
+  name = "Rahul";
+  render(){
+    return(
+      <div>
+        <h1>This is {this.name}!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
